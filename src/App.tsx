8a968b47177ee01087f739f0ad3679f765703d35
Ivan <ivanvolodin960@gmail.com>
2023-05-30 14:47:18 +0300

    feat: create component Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Board from './components/Board/Board';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout';
@@ -7,6 +8,9 @@ function App() {
   return (
     <Layout>
       <Header />
+      <main>
+        <Board />
+      </main>
       <Footer />
     </Layout>
   )
