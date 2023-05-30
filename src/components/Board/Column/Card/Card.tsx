d3046ec2be9f9b12db070777f1fdd81dad1393de
Ivan <ivanvolodin960@gmail.com>
2023-05-30 18:24:34 +0300

    feat: create generator of icons

--- src/components/Board/Column/Card/Card.tsx
+++ src/components/Board/Column/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GlobalSvgSelector } from '../../../Shared/icons/GlobalSvgSelector';
 import css from './Card.module.scss';
 
 interface ICardProps {
@@ -9,6 +10,7 @@ export default function Card({ name }: ICardProps) {
   return (
     <div className={css.card}>
       <span>{name}</span>
+      <GlobalSvgSelector id='close' />
     </div>
   )
 };
