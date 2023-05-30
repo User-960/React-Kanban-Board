a92f64866cc816d4208f7ac56cc4d5bff0d0af80
Ivan <ivanvolodin960@gmail.com>
2023-05-30 14:38:28 +0300

    feat: create component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <Layout>
+      <Header />
       <Footer />
     </Layout>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={css.header}>
+      Header
+    </header>
+  )
+};
