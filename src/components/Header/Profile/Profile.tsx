edc1e1e8192e5cfee0d565c3b6f259ab1d2601e2
Ivan <ivanvolodin960@gmail.com>
2023-06-01 17:20:42 +0300

    feat: add styles for component Profile

--- src/components/Header/Profile/Profile.tsx
+++ src/components/Header/Profile/Profile.tsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import { GlobalSvgSelector } from '../../Shared/icons/GlobalSvgSelector';
+import css from './Profile.module.scss';
 
 export default function Profile() {
   const [isMenuShown, setIsMenuShown] = useState<boolean>(false);
 
   return (
-    <div onClick={() => setIsMenuShown(!isMenuShown)}>
+    <div className={css['profile']} onClick={() => setIsMenuShown(!isMenuShown)}>
       <GlobalSvgSelector id="profile" />
-      <div>
+      <div className={`${css['chevron']} ${isMenuShown ? css['up'] : ''}`}>
         <GlobalSvgSelector id="chevron" />
       </div>
 
       {isMenuShown &&
-        <div>
-          <div>Profile</div>
-          <div>Log Out</div>
-        </div>
+        <menu className={css['menu']}>
+          <div className={css['menu-item']}>Profile</div>
+          <div className={css['menu-item']}>Log Out</div>
+        </menu>
       }
     </div>
   )
